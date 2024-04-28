14a71b17bc2ac4d0cd31d0c508717440d5c7413d
xnatsuk <xnatsuk@gmail.com>
2024-04-27 23:52:00 -0300

    feat: create client

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Main from './views/Main'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Topics.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Topics() {
+  const [topics, setTopics] = useState([])
+  const [mostPopular, setMostPopular] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/topics', { method: 'GET' })
+      .then(response => response.json())
+      .then(data => {
+        setTopics(data.topics)
+        setMostPopular(getPopular(data.topics))
+      });
+  }, []);
+
+  const getPopular = (topics) => {
+    return topics.reduce((mostPopular, currentTopic) => {
+      const currentPop = currentTopic.likes + currentTopic.messages
+      const mostPop = mostPopular.likes + mostPopular.messages
+
+      return currentPop > mostPop ? currentTopic : mostPopular
+    }, topics[0])
+  }
+
+  return (
+    <div className="topics">
+      <h2>Popular no Momento</h2>
+      <div className='popular'>
+        <h3>
+          {mostPopular
+            ? mostPopular.title
+            : 'Nenhum tópico popular no momento'
+          }
+        </h3>
+        <p className='topic-info'>{mostPopular?.description}</p>
+        <p className='topic-stats'>Likes: {mostPopular?.likes}</p>
+        <p className='topic-stats'>Mensagens: {mostPopular?.messages}</p>
+      </div>
+
+
+      <h2>Todos os Tópicos</h2>
+      {topics.map((topic, i) => (
+        <ul>
+          <li key={i} className='topics-item'>
+            <h3>{topic.title}</h3>
+            <p className='topic-info'>{topic.description}</p>
+            <p className='topic-stats'>Likes: {topic.likes}</p>
+            <p className='topic-stats'>Mensagens: {topic.messages}</p>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+}
