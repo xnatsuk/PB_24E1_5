c79defb6c7bfc327c3dd9f521c997de18af43691
xnatsuk <xnatsuk@gmail.com>
2024-04-29 23:32:07 -0300

    chore(css): add tailwindcss for styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/router'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
