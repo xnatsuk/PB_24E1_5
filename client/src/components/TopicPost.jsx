fc6b562b77be4a783528eabacb2f9dc758327953
xnatsuk <xnatsuk@gmail.com>
2024-04-29 23:45:35 -0300

    feat: add topic page and user card component

---
+++ client/src/components/TopicPost.jsx
@@ -0,0 +1,33 @@
+export const TopicPost = ({ post }) => (
+  <>
+    <div className="flex flex-col justify-between overflow-hidden p-5 w-full border-t border-orange-300 shadow-2xl rounded-lg">
+      <div>
+        <h1 className="px-4 py-2 text-2xl text-orange-400">
+          {post.title}
+        </h1>
+
+        <div className="inline-flex px-4">
+          <p >
+            {post.description}
+          </p>
+        </div>
+      </div >
+
+      <div className="mx-auto px-4 my-10">
+        <p>
+          {post.content}
+        </p>
+      </div>
+
+      <div className="flex justify-between md:justify-around mt-2 ">
+        <p>
+          Likes: {post.likes}
+        </p>
+        <p>
+          Mensagens: {post.messages}
+        </p>
+      </div>
+
+    </div >
+  </>
+)
--- client/src/views/Topic.jsx
+++ client/src/views/Topic.jsx
@@ -1,9 +1,24 @@
-// import { TopicPage } from '../components';
+import { useState } from 'react';
+import { UserCard } from '../components/UserCard';
+import { TopicPost } from '../components/TopicPost';
 
-export default function Topic() {
+export const Topic = () => {
+  //exemplo para visualização
+  const [user, setUser] = useState({ name: 'John Doe' })
+  const [post, setPost] = useState({
+    title: 'Título do Tópico',
+    description: 'Breve descrição do tópico',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    likes: 100,
+    messages: 20
+  })
+
   return (
     <>
-      {/* <TopicPage /> */}
+      <div className='flex flex-row gap-2 px-4 py-10 shadow-2xl rounded-lg'>
+        <UserCard user={user} />
+        <TopicPost post={post} />
+      </div>
     </>
   )
 }
