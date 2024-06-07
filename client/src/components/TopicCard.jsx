a9bc591821e0528abffd2926b29102bc9b73d0bd
xnatsuk <xnatsuk@gmail.com>
2024-06-07 14:24:25 -0300

    style: format code

--- client/src/components/TopicCard.jsx
+++ client/src/components/TopicCard.jsx
@@ -1,32 +1,38 @@
-import { Link } from "react-router-dom";
-import { transformToUrl } from '../utils';
+import { Link } from 'react-router-dom'
+import { transformToUrl } from '../utils'
 
-export const TopicCard = ({ title, description, likes, messages }) => (
-  <div className="container mx-auto px-4 py-5 mb-3 rounded-t bg-zinc-900 hover:bg-zinc-800 shadow-2xl divide-y divide-zinc-900 hover:divide-zinc-800">
-    <div className="flex items-center justify-between overflow-hidden px-5">
-      <div>
-        <Link
-          className="block px-4 py-2 text-xl text-orange-400"
-          to={`topic/${transformToUrl(title)}`}
-        >
-          {title}
-        </Link>
+export function TopicCard({ title, description, likes, messages }) {
+  return (
+    <div className="container mx-auto px-4 py-5 mb-3 rounded-t bg-zinc-900 hover:bg-zinc-800 shadow-2xl divide-y divide-zinc-900 hover:divide-zinc-800">
+      <div className="flex items-center justify-between overflow-hidden px-5">
+        <div>
+          <Link
+            className="block px-4 py-2 text-xl text-orange-400"
+            to={`topic/${transformToUrl(title)}`}
+          >
+            {title}
+          </Link>
 
-        <div className=" flex-shrink-0">
-          <p className="inline-flex px-2  leading-5 ml-2 self">
-            {description}
+          <div className=" flex-shrink-0">
+            <p className="inline-flex px-2  leading-5 ml-2 self">
+              {description}
+            </p>
+          </div>
+        </div>
+
+        <div className="mt-2 flex justify-between flex-col">
+          <p>
+            Likes:
+            {' '}
+            {likes}
+          </p>
+          <p>
+            Mensagens:
+            {' '}
+            {messages}
           </p>
         </div>
       </div>
-
-      <div className="mt-2 flex justify-between flex-col">
-        <p>
-          Likes: {likes}
-        </p>
-        <p>
-          Mensagens: {messages}
-        </p>
-      </div>
     </div>
-  </div>
-)
+  )
+}
--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -1,25 +1,22 @@
-export const UserCard = ({ user }) => {
+export function UserCard({ user }) {
   return (
-    <>
-      <div className="text-center max-w-60 shadow-2xl rounded-lg">
-        <img
-          className=" w-36 h-36 mx-auto -mb-24 rounded-full"
-          src="https://via.placeholder.com/150"
-          alt="user"
-        />
-        <div className="px-8 pt-32 pb-8 text-gray-200 bg-zinc-900 rounded-lg">
-          <h2 className="my-3 text-2xl text-gray-100">
-            {user.name}
-          </h2>
-          <p className="my-5 text-orange-300">
-            Titulo
-          </p>
-          <p>
-            Classificação de usuário
-          </p>
-        </div>
+    <div className="text-center max-w-60 shadow-2xl rounded-lg">
+      <img
+        className=" w-36 h-36 mx-auto -mb-24 rounded-full"
+        src="https://via.placeholder.com/150"
+        alt="user"
+      />
+      <div className="px-8 pt-32 pb-8 text-gray-200 bg-zinc-900 rounded-lg">
+        <h2 className="my-3 text-2xl text-gray-100">
+          {user.name}
+        </h2>
+        <p className="my-5 text-orange-300">
+          Titulo
+        </p>
+        <p>
+          Classificação de usuário
+        </p>
       </div>
-
-    </>
+    </div>
   )
 }
