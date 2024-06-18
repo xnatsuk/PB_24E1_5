8548b6364396e7cd114ed332f0bdc31d2abcbac7
xnatsuk <xnatsuk@gmail.com>
2024-06-18 01:31:19 -0300

    refactor: change router file location

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import router from './routes/router'
+import { router } from './routes/router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -1,6 +1,53 @@
-import { createBrowserRouter, } from 'react-router-dom'
-import { pages } from '../views/pages'
+import { createBrowserRouter, redirect } from 'react-router-dom'
+import { Layout } from '../layouts/default'
+import { Home } from '../views/Home'
+import { Post } from '../views/Post'
+import { Login } from '../views/Login'
+import { Register } from '../views/Register'
+import { AuthCard } from '../components/AuthCard'
+import { useAuthStore } from '../stores/auth.store'
+import ErrorPage from './error-page'
 
-const router = createBrowserRouter(pages)
+function authLoader() {
+  const token = useAuthStore.getState().session.access_token
+  if (!token)
+    return redirect('/auth/login')
+}
 
-export default router
+export const routes = [
+  {
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        id: 'root',
+        element: <Home />,
+      },
+      {
+        path: 'post/:id',
+        id: 'id',
+        element: <Post />,
+      },
+
+      {
+        path: '/auth',
+        element: <AuthCard />,
+        children: [
+          {
+            path: 'auth/login',
+            element: <Login />,
+
+          },
+          {
+            path: 'auth/register',
+            element: <Register />,
+          },
+        ],
+      },
+    ],
+  },
+
+]
+
+export const router = createBrowserRouter(routes)
