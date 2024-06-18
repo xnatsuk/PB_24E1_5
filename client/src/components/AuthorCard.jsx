ade81cb01b4e87ad7bd71fa47213c3b666eb846c
xnatsuk <xnatsuk@gmail.com>
2024-06-18 19:31:02 -0300

    feat: create base components

--- client/src/components/AuthorCard.jsx
+++ client/src/components/AuthorCard.jsx
@@ -1,24 +1,12 @@
-export function AuthorCard({ user }) {
+import { BaseAuthorCard } from './base/BaseAuthorCard'
+
+export function AuthorCard({ username, reputation, score, avatar_url }) {
   return (
-    <div className="container h-full text-center max-w-60 shadow-2xl rounded-lg">
-      <img
-        className=" w-36 h-36 mx-auto -mb-24 rounded-full"
-        src={user.avatar_url ?? 'https://via.placeholder.com/150'}
-        alt="user"
-      />
-      <div className="px-8 pt-32 pb-8 text-gray-200 bg-zinc-900 rounded-lg">
-        <h2 className="my-3 text-2xl text-gray-100">
-          {user.username}
-        </h2>
-        <p className="my-5 text-orange-300">
-          {user.reputation}
-        </p>
-        <p>
-          Pontuação:
-          {' '}
-          {user.score}
-        </p>
-      </div>
-    </div>
+    <BaseAuthorCard
+      username={username}
+      reputation={reputation}
+      score={score}
+      avatar_url={avatar_url}
+    />
   )
 }
--- client/src/components/PostDetail.jsx
+++ client/src/components/PostDetail.jsx
@@ -1,37 +1,42 @@
-export function PostDetail({ post }) {
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { postRequest } from '../services/posts'
+import { profileRequest } from '../services/profiles'
+import { AuthorCard } from '../components/AuthorCard'
+import { BasePostDetail } from './base/BasePostDetail'
+
+export function PostDetail() {
+  const [post, setPost] = useState({})
+  const [user, setUser] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+    postRequest.getById(id)
+      .then((response) => {
+        setPost(response)
+        profileRequest.getById(response.user_id)
+          .then(authorRes => setUser(authorRes))
+          .catch(console.error)
+      })
+      .catch(console.error)
+  }, [id])
+
   return (
-    <div className="flex flex-col justify-between overflow-hidden p-5 w-full border-t border-orange-300 shadow-2xl rounded-lg">
-      <div>
-        <h1 className="px-4 py-2 text-2xl text-orange-400">
-          {post.title}
-        </h1>
-
-        <div className="inline-flex px-4">
-          <p>
-            {post.description}
-          </p>
-        </div>
-      </div>
-
-      <div className="mx-auto px-4 my-10">
-        <p>
-          {post.content}
-        </p>
-      </div>
-
-      <div className="flex justify-between md:justify-around mt-2 ">
-        <p>
-          Likes:
-          {' '}
-          {post.likes}
-        </p>
-        <p>
-          Mensagens:
-          {' '}
-          {post.messages}
-        </p>
-      </div>
-
-    </div>
+    <BasePostDetail
+      title={post.title}
+      description={post.description}
+      content={post.content}
+      likes={post.likes}
+      messages={post.messages?.length()}
+      created_at={post.created_at}
+      updated_at={post.updated_at}
+    >
+      <AuthorCard
+        username={user.username}
+        reputation={user.reputation}
+        score={user.score}
+        avatar_url={user.avatar_url}
+      />
+    </BasePostDetail>
   )
 }
