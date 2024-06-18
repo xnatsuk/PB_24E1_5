af853d4ab278f5d52db7d7e9efb8d627c527e632
xnatsuk <xnatsuk@gmail.com>
2024-06-18 01:21:03 -0300

    refactor: remove auth dependencies

--- backend/src/schemas/__init__.py
+++ backend/src/schemas/__init__.py
@@ -1,5 +1,5 @@
-from src.schemas.auth_schema import Token, UserLogin, UserCreate, UserData, UserSession
-from src.schemas.base_schema import CreateBase, ResponseBase, UpdateBase
+from src.schemas.base_schema import CreateBase, ResponseBase, UpdateBase, InDbBase
 from src.schemas.message_schema import Messages, MessageCreate, MessageUpdate, MessageInDB
 from src.schemas.post_schema import Posts, PostCreate, PostUpdate, PostInDB
+from src.schemas.profile_schema import Profiles, ProfileUpdate, ProfileInDb, ProfileBase
 from src.schemas.supabase_schema import SupabaseDB
